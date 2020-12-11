370b31bc0d21c0ff0d4084e2ae266fe2437085b0
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2020-12-10 21:42:30 -0600

    render todos on refresh

--- script.js
+++ script.js
@@ -17,104 +17,96 @@ parseInt($('#hr6').attr('data-mlt')),
 parseInt($('#hr7').attr('data-mlt')),
 parseInt($('#hr8').attr('data-mlt')),];
 var eventInput = $('.description');
-// var todos = JSON.parse(localStorage.todo) || [];
+var arrayOfValues = Object.values(localStorage);
 currentDay.text(todaysDate);
 
 // control color coding
 function currentBlock() {
-    for (var i = 0; i < 8; i++) {
-      var presentTime = parseInt(moment().format('k'))
-         if (timeBlock[i] == presentTime) {
-            $(textBlock[i]).attr('class', 'present')
-         }  
-         if (timeBlock[i] > presentTime) {
-            $(textBlock[i]).attr('class', 'future')
-         }
-         if (timeBlock[i] < presentTime) {
+  for (var i = 0; i < 8; i++) {
+    var presentTime = parseInt(moment().format('k'))
+      if (timeBlock[i] == presentTime) {
+          $(textBlock[i]).attr('class', 'present')
+        }  
+      if (timeBlock[i] > presentTime) {
+          $(textBlock[i]).attr('class', 'future')
+        }
+      if (timeBlock[i] < presentTime) {
           $(textBlock[i]).attr('class', 'past')
-         }
+        } 
+      textBlock[i].text(arrayOfValues[i])       
     }
 }
 currentBlock()
 // button function to store todos
 function newEvent1() {
   var newTodo = $(this).prev().val();
-  if (newTodo !== '') {
     localStorage.todo1 = JSON.stringify(newTodo);
     $('#txt1').val('')
     $('#txt1').val(JSON.parse(localStorage.todo1)) 
-  }
 }
 
 $('#btn1').click (newEvent1)
 
 function newEvent2() {
-  $('#txt2').val('')
   var newTodo = $(this).prev().val();
-  if (newTodo !== '') {
     localStorage.todo2 = JSON.stringify(newTodo);
-  }
+    $('#txt2').val('')
+    $('#txt2').val(JSON.parse(localStorage.todo2))
 }
 
 $('#btn2').click (newEvent2)
 
 
 function newEvent3() {
-  $('#txt3').val('')
   var newTodo = $(this).prev().val();
-  if (newTodo !== '') {
     localStorage.todo3 = JSON.stringify(newTodo);
-  }
+    $('#txt3').val('')
+    $('#txt3').val(JSON.parse(localStorage.todo3))
 }
 
 $('#btn3').click (newEvent3)
 
 function newEvent4() {
-  $('#txt4').val('')
   var newTodo = $(this).prev().val();
-  if (newTodo !== '') {
     localStorage.todo4 = JSON.stringify(newTodo);
-  }
+    $('#txt4').val('')
+    $('#txt4').val(JSON.parse(localStorage.todo4))
 }
 
 $('#btn4').click (newEvent4)
 
 function newEvent5() {
-  $('#txt5').val('')
   var newTodo = $(this).prev().val();
-  if (newTodo !== '') {
     localStorage.todo5 = JSON.stringify(newTodo);
-  }
+    $('#txt5').val('')
+    $('#txt5').val(JSON.parse(localStorage.todo5))
 }
 
 $('#btn5').click (newEvent5)
 
 function newEvent6() {
-  $('#txt6').val('')
-  var newTodo = $(this).prev().val();
-  if (newTodo !== '') {
+  var newTodo = $(this).prev().val()
     localStorage.todo6 = JSON.stringify(newTodo);
-  }
+    $('#txt6').val('')
+    $('#txt6').val(JSON.parse(localStorage.todo6))
 }
 
 $('#btn6').click (newEvent6)
 
 function newEvent7() {
-  $('#txt7').val('')
   var newTodo = $(this).prev().val();
-  if (newTodo !== '') {
     localStorage.todo7 = JSON.stringify(newTodo);
-  }
+    $('#txt7').val('')
+    $('#txt7').val(JSON.parse(localStorage.todo7))
 }
 
 $('#btn7').click (newEvent7)
 
 function newEvent8() {
-  $('#txt8').val('')
   var newTodo = $(this).prev().val();
-  if (newTodo !== '') {
     localStorage.todo8 = JSON.stringify(newTodo);
-  }
+    $('#txt8').val('')
+    $('#txt8').val(JSON.parse(localStorage.todo8))
 }
 
 $('#btn8').click (newEvent8)
