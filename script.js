64732c57207a7c76fd786c592cdcf967a34f4a93
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2020-12-12 14:23:04 -0600

    finalize refresh function

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ var arrayOfKeys = Object.keys(localStorage);
 var orderedKeys = arrayOfKeys.sort();
 
 currentDay.text(todaysDate);
-console.log(orderedKeys)
+
 // control color coding
 function currentBlock() {
   for (var i = 0; i < 8; i++) {
@@ -54,86 +54,86 @@ function renderTodos() {
       todo8: 8,
     }
   todoNum = translators[orderedKeys[i]]
-    // console.log(JSON.parse(localStorage.getItem(orderedKeys[i])))
-    console.log(todoNum) 
-    // console.log(textBlock[i].attr('data-index') == todoNum)
-    //  {
-    //   textBlock[i].val(blockText)
-    // }
+    blockText = localStorage.getItem(orderedKeys[i])
+    // console.log(todoNum) 
+    if (textBlock[i].attr('data-index') == todoNum)
+     {
+      textBlock[i].val(blockText)
+    }
   }
 }
 renderTodos()
        
     // button function to store todos
 function newEvent1() {
-  var newTodo = $(this).prev().val();
+  var newTodo = $(this).prev().val()+ ' noted: ' + moment().format('dddd, MMMM Do') ;
     localStorage.todo1 = JSON.stringify(newTodo);
     $('#txt1').val('')
-    $('#txt1').val(JSON.parse(localStorage.todo1) + ' noted: ' + moment().format('dddd, MMMM Do')) 
+    $('#txt1').val(JSON.parse(localStorage.todo1)) 
 }
 
 $('#btn1').click (newEvent1)
 
 function newEvent2() {
-  var newTodo = $(this).prev().val();
+  var newTodo = $(this).prev().val() + ' noted: ' + moment().format('dddd, MMMM Do');
     localStorage.todo2 = JSON.stringify(newTodo);
     $('#txt2').val('')
-    $('#txt2').val(JSON.parse(localStorage.todo2) + ' noted: ' + moment().format('dddd, MMMM Do'))
+    $('#txt2').val(JSON.parse(localStorage.todo2))
 }
 
 $('#btn2').click (newEvent2)
 
 
 function newEvent3() {
-  var newTodo = $(this).prev().val();
+  var newTodo = $(this).prev().val() + ' noted: ' + moment().format('dddd, MMMM Do');
     localStorage.todo3 = JSON.stringify(newTodo);
     $('#txt3').val('')
-    $('#txt3').val(JSON.parse(localStorage.todo3) + ' noted: ' + moment().format('dddd, MMMM Do'))
+    $('#txt3').val(JSON.parse(localStorage.todo3))
 }
 
 $('#btn3').click (newEvent3)
 
 function newEvent4() {
-  var newTodo = $(this).prev().val();
+  var newTodo = $(this).prev().val() + ' noted: ' + moment().format('dddd, MMMM Do');
     localStorage.todo4 = JSON.stringify(newTodo);
     $('#txt4').val('')
-    $('#txt4').val(JSON.parse(localStorage.todo4) + ' noted: ' + moment().format('dddd, MMMM Do'))
+    $('#txt4').val(JSON.parse(localStorage.todo4))
 }
 
 $('#btn4').click (newEvent4)
 
 function newEvent5() {
-  var newTodo = $(this).prev().val();
+  var newTodo = $(this).prev().val() + ' noted: ' + moment().format('dddd, MMMM Do');
     localStorage.todo5 = JSON.stringify(newTodo);
     $('#txt5').val('')
-    $('#txt5').val(JSON.parse(localStorage.todo5) + ' noted: ' + moment().format('dddd, MMMM Do'))
+    $('#txt5').val(JSON.parse(localStorage.todo5))
 }
 
 $('#btn5').click (newEvent5)
 
 function newEvent6() {
-  var newTodo = $(this).prev().val()
+  var newTodo = $(this).prev().val() + ' noted: ' + moment().format('dddd, MMMM Do');
     localStorage.todo6 = JSON.stringify(newTodo);
     $('#txt6').val('')
-    $('#txt6').val(JSON.parse(localStorage.todo6) + ' noted: ' + moment().format('dddd, MMMM Do'))
+    $('#txt6').val(JSON.parse(localStorage.todo6))
 }
 
 $('#btn6').click (newEvent6)
 
 function newEvent7() {
-  var newTodo = $(this).prev().val();
+  var newTodo = $(this).prev().val() + ' noted: ' + moment().format('dddd, MMMM Do');
     localStorage.todo7 = JSON.stringify(newTodo);
     $('#txt7').val('')
-    $('#txt7').val(JSON.parse(localStorage.todo7) + ' noted: ' + moment().format('dddd, MMMM Do'))
+    $('#txt7').val(JSON.parse(localStorage.todo7))
 }
 
 $('#btn7').click (newEvent7)
 
 function newEvent8() {
-  var newTodo = $(this).prev().val();
+  var newTodo = $(this).prev().val() + ' noted: ' + moment().format('dddd, MMMM Do');
     localStorage.todo8 = JSON.stringify(newTodo);
     $('#txt8').val('')
-    $('#txt8').val(JSON.parse(localStorage.todo8) + ' noted: ' + moment().format('dddd, MMMM Do'))
+    $('#txt8').val(JSON.parse(localStorage.todo8))
 }
 
 $('#btn8').click (newEvent8)
@@ -142,13 +142,13 @@ function clearEvents() {
   for (var i = 0; i < 8; i++){
     textBlock[i].val('')
   }
-  localStorage.todo1 = 'Schedule Events';
-  localStorage.todo2 = 'Schedule Events';
-  localStorage.todo3 = 'Schedule Events';
-  localStorage.todo4 = 'Schedule Events';
-  localStorage.todo5 = 'Schedule Events';
-  localStorage.todo6 = 'Schedule Events';
-  localStorage.todo7 = 'Schedule Events';
-  localStorage.todo8 = 'Schedule Events';
+  localStorage.todo1 = '';
+  localStorage.todo2 = '';
+  localStorage.todo3 = '';
+  localStorage.todo4 = '';
+  localStorage.todo5 = '';
+  localStorage.todo6 = '';
+  localStorage.todo7 = '';
+  localStorage.todo8 = '';
 }
 $('#delete-all').click(clearEvents)
